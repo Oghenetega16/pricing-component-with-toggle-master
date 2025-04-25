cb5628b24f36e5d7c38e0ca0a5029b2ec518c6e0
oghenetega16 <sukuruoghenetega@gmail.com>
2025-04-25 22:09:55 +0100

    In progress

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,8 +1,27 @@
-export default function PricingCard() {
+export default function PricingCard({
+    plan = '',
+    price = '',
+    storage = '',
+    users = '',
+    send = '',
+    cardBgColor = 'bg-white',
+    cardTextColor = 'text-dark-grayish-blue',
+    buttonBgColor = 'bg-gradient-to-br from-[hsl(236,72%,79%)] to-[hsl(237,63%,64%)]',
+    buttonTextColor = 'text-white'
+}) {
     return (
-        <div className="bg-white text-center">
-            <p className="text-xl font-medium">Basic</p>
-            <h1 className="text-6xl font-bold pb-8 border "><span className="text-xl">$</span>19.99</h1>
+        <div className={`${cardBgColor} text-center ${cardTextColor} font-bold rounded-xl px-7 py-10 mb-8`}>
+            <p className="text-xl mb-8">{plan}</p>
+            <div className="flex items-center justify-center text-7xl pb-8 border-b border-gray-200">
+                <span className="text-4xl">$</span>
+                <h1>{price}</h1>
+            </div>
+            <ul>
+                <li className="py-4 border-b border-gray-200">{storage}</li>
+                <li className="py-4 border-b border-gray-200">{users}</li>
+                <li className="py-4 border-b border-gray-200">{send}</li>
+            </ul>
+            <button className={`uppercase ${buttonBgColor} ${buttonTextColor} flex justify-center w-full mt-8 py-3 rounded-lg`}>Learn More</button>
         </div>
     )
 }
--- src/components/PricingPage.jsx
+++ src/components/PricingPage.jsx
@@ -1,18 +1,56 @@
-
+import { useState } from "react"
 import PricingCard from "./PricingCard"
 export default function PricingPage() {
+    const [toggle, setToggle] = useState(true)
+
+    function handleToggle() {
+        setToggle(!toggle)
+    }
+
     return (
-        <div className="min-h-screen bg-grayish-blue bg-[url(/images/bg-top.svg)] bg-contain bg-top-right bg-no-repeat">
+        <div className="min-h-screen bg-grayish-blue">
             <div className="font-display text-center py-12 px-6">
                 <h1 className="text-4xl font-bold text-gray-blue mb-8">Our Pricing</h1>
                 <div className="flex items-center justify-center space-x-8 text-light-grayish-blue font-bold mb-20">
                     <p>Annually</p>
-                    <img src="/images/toggle-on.png" alt="" />
+                    <img 
+                        src={toggle ? '/images/toggle-on.png' : '/images/toggle-off.png'} 
+                        alt="toggle icon" 
+                        onClick={() => handleToggle()}
+                        className="cursor-pointer"
+                    />
                     <p>Monthly</p>
                 </div>
-
-                <PricingCard />
+                <div className="flex justify-center flex-col md:flex-row">
+                    <PricingCard 
+                        plan = 'Basic'
+                        price = {toggle ? '19.99' : '199.99'}
+                        storage = '500 GB Storage'
+                        users = '2 Users Allowed'
+                        send = 'Send up to 3 GB'
+                    />
+                    <PricingCard 
+                        plan="Professional"
+                        price={toggle ? '24.99' : '249.99'}
+                        storage="1TB Storage"
+                        users="5 Users Allowed"
+                        send="Send up to 10 GB"
+                        cardBgColor="bg-gradient-to-tr from-[hsl(236,72%,79%)] to-[hsl(237,63%,64%)]"
+                        cardTextColor="text-white"
+                        buttonBgColor="bg-white"
+                        buttonTextColor=""
+                    />
+                    <PricingCard 
+                        plan="Master" 
+                        price={toggle ? '39.99' : '399.99'}
+                        storage="2TB Storage" 
+                        users="10 Users Allowed" 
+                        send="Send up to 20 GB"
+                    />
+                </div>
             </div>
         </div>
     )
 }
+
+// bg-[url(/images/bg-top.svg)] bg-contain bg-top-right bg-no-repeat
